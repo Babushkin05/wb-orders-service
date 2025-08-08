aec5be06eec41c22fc59bcfd84d70aa5b7b11ce3
Vova Babushkin <babushkinvova05@gmail.com>
2025-08-08 19:52:59 +0700

    add method WarmUp for cacher

--- internal/application/cacher.go
+++ internal/application/cacher.go
@@ -5,4 +5,5 @@ import "github.com/Babushkin05/wb-orders-service/internal/domain/model"
 type Cacher interface {
 	Cache(order *model.Order) error
 	GetOrderFromCache(orderUID string) (*model.Order, error)
+	WarmUp() error
 }
